d36404f40ba0d9322b1e34c15eb93daebcabc3c2
Glypse <glypse.designs@gmail.com>
2024-01-11 21:31:03 +0100

    AV0.7
    Deeply reworked the cursor, the Instagram gradient properly animates, some stuff. REWORK BROWSER COMPATIBILITY

--- script.js
+++ script.js
@@ -2,56 +2,69 @@
 
 if (matchMedia('(pointer:fine)').matches) {
   // Device has a mouse
-  var cursorSize = 24;
-  const hover = document.querySelectorAll('a');
 
-  const coords = { x: 0, y: 0 };
-  const circles = document.querySelectorAll(".cursorCircle");
+  //Create cursor divs
 
-  circles.forEach(function (cursorCircle, index) {
-    cursorCircle.x = 0;
-    cursorCircle.y = 0;
-  });
+  document.addEventListener('DOMContentLoaded', function() {
+    // Create the cursorGroup div
+    var cursorGroupScript = document.createElement('div');
+    cursorGroupScript.classList.add('cursorGroup');
 
-  window.addEventListener("mousemove", function(e){
-    coords.x = e.clientX;
-    coords.y = e.clientY;
-    
-  });
+    // Create the cursorCircle0 div
+    var cursorCircle0Script = document.createElement('div');
+    cursorCircle0Script.classList.add('cursorCircle', 'cursorCircle0');
 
-  function animateCircles() {
-    
-    let x = coords.x;
-    let y = coords.y;
-    
+    // Append cursorCircle0 to cursorGroup
+    cursorGroupScript.appendChild(cursorCircle0Script);
+
+    // Create 20 cursorCircleS divs and append them to cursorGroup
+    for (var i = 1; i <= 80; i++) {
+      var cursorCircleScript = document.createElement('div');
+      cursorCircleScript.classList.add('cursorCircle');
+      cursorGroupScript.appendChild(cursorCircleScript);
+    }
+
+    // Get the body element
+    var body = document.body;
+
+    // Insert cursorGroup as the first child of the body
+    body.insertBefore(cursorGroupScript, body.firstChild);
+
+    // Animate mouse
+    var cursorSize = 24;
+    const coords = { x: 0, y: 0 };
+    const circles = document.querySelectorAll(".cursorCircle");
+
     circles.forEach(function (cursorCircle, index) {
-      cursorCircle.style.left = x - (cursorSize/2) + "px";
-      cursorCircle.style.top = y - (cursorSize/2) + "px";
-      cursorCircle.style.setProperty('--cursorSize', cursorSize + "px");
-      
-      cursorCircle.style.scale = (circles.length - index) / circles.length;
-      
-      cursorCircle.x = x;
-      cursorCircle.y = y;
-
-      const nextCircle = circles[index + 1] || circles[0];
-      x += (nextCircle.x - x) * 0.1;
-      y += (nextCircle.y - y) * 0.1;
+      cursorCircle.x = 0;
+      cursorCircle.y = 0;
     });
-  
-    requestAnimationFrame(animateCircles);
-  }
 
-  animateCircles();
-} else {
-  // Device doesn't have a mouse
-  var cursorSize = 0;
-  const coords = { x: 0, y: 0 };
-  const circles = document.querySelectorAll(".cursorCircle");
-  function animateCircles() {
-    circles.forEach(function (cursorCircle, index) {
-      cursorCircle.style.setProperty('--cursorSize', cursorSize + "px");
+    window.addEventListener("mousemove", function(e){
+      coords.x = e.clientX;
+      coords.y = e.clientY;
     });
-  }
-  animateCircles();
+
+    function animateCircles() {
+      let x = coords.x;
+      let y = coords.y;
+      
+      circles.forEach(function (cursorCircle, index) {
+        cursorCircle.style.left = x - (cursorSize / 2) + "px";
+        cursorCircle.style.top = y - (cursorSize / 2) + "px";
+        cursorCircle.style.setProperty('--cursorSize', cursorSize + "px");
+
+        cursorCircle.x = x;
+        cursorCircle.y = y;
+
+        const nextCircle = circles[index + 1] || circles[0];
+        x += (nextCircle.x - x) * 0.075;
+        y += (nextCircle.y - y) * 0.075;
+      });
+
+      requestAnimationFrame(animateCircles);
+    }
+
+    animateCircles();
+  });
 }
